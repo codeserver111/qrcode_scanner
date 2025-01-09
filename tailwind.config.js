a1593efbd479023e35b11a40bc6de07e38bb9830
user2 <codeserver111@gmail.com>
2025-01-09 20:41:03 +0530

    issue fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -70,6 +70,10 @@ module.exports = {
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
 			},
+			fontFamily: {
+				geist: ['Geist', 'sans-serif'],
+				geistMono: ['Geist Mono', 'monospace'],
+			  },
 		},
 	},
 	plugins: [require("tailwindcss-animate")],
