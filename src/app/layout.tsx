e2ca72590677ab55495e5563c719bb9997d7b161
user2 <codeserver111@gmail.com>
2025-01-09 21:22:22 +0530

    issue fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,12 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 const geistSans = localFont({
-  src: "../app/fonts/GeistSans.woff",
+  src: "./fonts/GeistSans.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "../app/fonts/GeistMonoVF.woff",
+  src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
